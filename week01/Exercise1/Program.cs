4dcc3d38464622acf5aa0cd4f241f84e507de4a3
mparra26 <mparramendoza@byupathway>
2025-05-09 10:46:36 -0400

    update

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("Enter your first name: Mikel");
+        string firstName = Console.ReadLine();
+
+        
+        Console.Write("Enter your last name: Parra");
+        string lastName = Console.ReadLine();
+
+    
+        Console.WriteLine($"Your name is: Parra Mikel");
     }
 }
